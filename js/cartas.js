b5f4f2c936a173a619bf81b476cf0035d9c9c28a
Julian Ascanio <julianandresac@ufps.edu.co>
2024-04-21 21:14:40 -0500

    CSS y js de juego de cartas

---
+++ js/cartas.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+    $('#guardar').click(function() {
+        guardarCarta();
+        pintarTabla();
+    });
+
+    $('.btncarta').click(function() {
+        var datos = localStorage.getItem('datos');
+        datos = JSON.parse(datos);
+
+        for (let item of datos) {
+            if (item.numero == $(this).data('carta')) {
+                item.cantidad++;
+            }
+        }
+
+        localStorage.setItem('datos', JSON.stringify(datos));
+        pintarTabla();
+    });
+
+    function guardarCarta() {
+        var numero = document.querySelector('#numero').value;
+        var carta = document.querySelector('#carta').value;
+        var datos = localStorage.getItem('datos');
+
+        datos = JSON.parse(datos) || [];
+
+        var dato = { numero: numero, carta: carta, cantidad: '0' };
+
+        datos.push(dato);
+
+        localStorage.setItem('datos', JSON.stringify(datos));
+    }
+
+    function cargarJSON() {
+        var miObjeto = [
+            { 'numero': '1', 'carta': 'as', 'cantidad': '0' },
+            { 'numero': '2', 'carta': '2 de diamantes', 'cantidad': '0' }
+        ];
+
+        localStorage.setItem('datos', JSON.stringify(miObjeto));
+    }
+
+    function pintarTabla() {
+        var datos = localStorage.getItem('datos');
+        let res = document.querySelector('#listaCartas');
+        res.innerHTML = '';
+
+        console.log('objetoObtenido: ', JSON.parse(datos));
+
+        datos = JSON.parse(datos);
+
+        for(let item of datos) {
+            res.innerHTML += `<tr>
+                <td>${item.numero}</td>
+                <td>${item.carta}</td>
+                <td>${item.cantidad}</td>
+            </tr>`
+        }
+    }
+
+    function leerJSON() {
+        $.getJSON("../data/datos.json", function (datos) {
+            
+        })
+            console.log(datos)
+            .catch(error => console.error('Error al leer el archivo JSON:', error));
+    }
+
+    leerJSON();
+    cargarJSON();
+    pintarTabla();
+});
+
+
